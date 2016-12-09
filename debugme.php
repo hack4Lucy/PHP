49b0e3bd403893640e6b5eae61ea7c9bf6e0cd8c
Hailemichael M. Worku <hack4Lucy@gmail.com>
2016-12-09 21:30:53 +0100

    Update debugme.php

--- debugme.php
+++ debugme.php
@@ -1,1 +1,14 @@
 
+// [Fri, 20161209]. How to use ISSET and EMPTY function in PHP during INSERTING/DELETING/DISPLAYING 
+//                  records from MySQL database. ;
+   
+   // (1). Usage of ISSET function
+   <?php 
+     if (isset($_POST['add')) {
+       // insert code here 
+     }           
+   ?>
+
+   <input type="submit", id="add", name="add" value="ADD RECORD">    // The ISSET function doesn't work in this case since only NAME parameter is used.
+   <input type="submit", name="add" value="ADD RECORD">              // The ISSET function works in this case because ID is used.
+   
